0b87341d5203303d82db31339f0abeababfaa3cf
Artem <kypidon71@gmail..com>
2022-12-10 16:11:09 +0300

    готова первая

---
+++ homeWork.py
@@ -0,0 +1,21 @@
+# оптимизация кода !!! три задачки
+#                                                                       Задачка номер 1
+                                                                            #было
+# num = ( 1,2,3,4,5,6,7)
+# day = int(input("add day = "))
+# if day <= 5:
+#     print("Budnie dni")
+# else:
+#     print("Gul8nka")
+                                                                            # стало
+# num = ('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье')
+# num_index = ( 1,2,3,4,5,6,7)
+
+# for num_index, num in zip(num_index, num):
+#     print(num_index, num)
+
+# day = int(input("add day = "))
+# if day <= 5:
+#     print(F'будний день пока поработай:= {day}')
+# else:
+#     print(f'отлично сегодня выходной:= {day}')
